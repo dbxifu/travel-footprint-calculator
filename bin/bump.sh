cd21cb1e23eab5e5d69a4ce862ff40873d28c255
Goutte <antoine@goutenoir.com>
2019-10-16 13:35:33 +0200

    Add a script to bump the semantic versioning from git description.

---
+++ bin/bump.sh
@@ -0,0 +1,24 @@
+#!/usr/bin/env bash
+
+# Fill the VERSION file with the new version, following semantic versioning.
+# You MUST run this script from the project's root directory.
+
+# COLORS ######################################################################
+
+Off='\033[0m'             # Text Reset
+
+# Regular Colors
+Black='\033[0;30m'        # Black
+Red='\033[0;31m'          # Red
+Green='\033[0;32m'        # Green
+Yellow='\033[0;33m'       # Yellow
+Blue='\033[0;34m'         # Blue
+Purple='\033[0;35m'       # Purple
+Cyan='\033[0;36m'         # Cyan
+White='\033[0;37m'        # White
+
+# BUSINESS ####################################################################
+
+VERSION=`git describe --tags`
+echo -e "${Green}Setting new version : ${Yellow}${VERSION}${Off}"
+echo ${VERSION} > VERSION
