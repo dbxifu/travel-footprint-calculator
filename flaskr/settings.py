b68964fef264cfe0e4dc3b03d290d46aa99a3d91
Goutte <antoine@goutenoir.com>
2019-10-16 20:02:25 +0200

    Add the initial settings.  More work on this is required.

---
+++ flaskr/settings.py
@@ -0,0 +1,41 @@
+import tempfile
+db_file = tempfile.NamedTemporaryFile()
+
+
+class Config(object):
+    SECRET_KEY = 'TWOOXYGENONECARBON'
+    # FLASK_RUN_EXTRA_FILES="content.yml"
+
+
+class ProductionConfig(Config):
+    ENV = 'production'
+    SQLALCHEMY_DATABASE_URI = 'sqlite:///../database.db'
+    SQLALCHEMY_TRACK_MODIFICATIONS = False
+
+    CACHE_TYPE = 'simple'
+    SECRET_KEY = 'REPLACE M3'  # todo: use a .env file
+
+
+class DevelopmentConfig(Config):
+    ENV = 'development'
+    DEBUG = True
+    DEBUG_TB_INTERCEPT_REDIRECTS = False
+
+    SQLALCHEMY_DATABASE_URI = 'sqlite:///../database.db'
+    SQLALCHEMY_TRACK_MODIFICATIONS = False
+
+    CACHE_TYPE = 'null'
+    ASSETS_DEBUG = True
+
+
+class TestConfig(Config):
+    ENV = 'test'
+    DEBUG = True
+    DEBUG_TB_INTERCEPT_REDIRECTS = False
+
+    SQLALCHEMY_DATABASE_URI = 'sqlite:///' + db_file.name
+    SQLALCHEMY_ECHO = True
+    SQLALCHEMY_TRACK_MODIFICATIONS = False
+
+    CACHE_TYPE = 'null'
+    WTF_CSRF_ENABLED = False
