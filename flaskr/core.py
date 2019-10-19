e0a7b516d9c9a8e56ec61eaaff72e19fdff5d404
Goutte <antoine@goutenoir.com>
2019-10-19 12:49:18 +0200

    Try out ABC.

--- flaskr/core.py
+++ flaskr/core.py
@@ -1,3 +1,4 @@
+import abc
 from datetime import datetime
 from uuid import uuid4
 
@@ -7,3 +8,13 @@ def generate_unique_id():
     :return: a unique identifier that can be sorted chronologically.
     """
     return datetime.now().strftime('%Y-%m-%d_%H:%M:%S_') + str(uuid4())[0:4]
+
+
+# def estimate_travel_carbon_footprint(from_address, to_address):
+#     return 1
+
+
+class FootprintEstimatorDriver(abc.ABCMeta):
+    @abc.abstractmethod
+    def get_travel_footprint(self, from_location, to_location):  # TBD
+        pass
