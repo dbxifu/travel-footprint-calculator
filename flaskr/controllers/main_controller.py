51f564d3eb366d7f6b241f3d919bdfe441233691
Antoine Goutenoir <antoine.goutenoir@irap.omp.eu>
2019-10-22 03:23:13 +0200

    Add a big chunk of the estimation logic.

--- flaskr/controllers/main_controller.py
+++ flaskr/controllers/main_controller.py
@@ -1,3 +1,5 @@
+import importlib
+
 from flask import Blueprint, render_template, flash, request, redirect, url_for
 
 from flaskr.extensions import cache
@@ -6,6 +8,7 @@ from flaskr.models import db, User, Estimation, StatusEnum
 from flaskr.geocoder import CachedGeocoder
 
 from flaskr.core import generate_unique_id
+from flaskr.content import content
 
 import sqlalchemy
 import geopy
@@ -47,7 +50,7 @@ def estimate():
 
 
 @main.route("/compute")
-def compute():
+def compute():  # process the queue of estimation requests
 
     def _respond(_msg):
         return "<pre>%s</pre>" % _msg
@@ -62,20 +65,58 @@ def compute():
         estimation = Estimation.query \
             .filter_by(status=StatusEnum.pending) \
             .order_by(Estimation.id.desc()) \
-            .one()
+            .first()
     except sqlalchemy.orm.exc.NoResultFound:
         return _respond("No estimation in the queue.")
+    except Exception as e:
+        return _respond("Database error: %s" % (e,))
+
+    assert estimation
 
     response += u"Processing estimation `%s` of `%s`...\n" % (estimation.id, estimation.email)
 
     geocoder = CachedGeocoder()
+
+    # GEOCODE ORIGINS #########################################################
+
+    origins_addresses = estimation.origin_addresses.split("\n")
+    origins = []
+
+    for i in range(len(origins_addresses)):
+
+        origin_address = str(origins_addresses[i]).strip()
+
+        try:
+            origin = geocoder.geocode(origin_address)
+        except geopy.exc.GeopyError as e:
+            response += u"Failed to geolocalize origin `%s`.\n%s" % (
+                origin_address, e,
+            )
+            _handle_failure(estimation, response)
+            return _respond(response)
+
+        if origin is None:
+            response += u"Failed to geolocalize origin `%s`." % (
+                origin_address,
+            )
+            _handle_failure(estimation, response)
+            return _respond(response)
+
+        origins.append(origin)
+
+        response += u"Origin: %s == %s (%f, %f)\n" % (
+            origin_address, origin.address,
+            origin.latitude, origin.longitude,
+        )
+
+    # GEOCODE DESTINATIONS ####################################################
 
     destinations_addresses = estimation.destination_addresses.split("\n")
     destinations = []
 
     for i in range(len(destinations_addresses)):
 
-        destination_address = str(destinations_addresses[i])
+        destination_address = str(destinations_addresses[i]).strip()
 
         try:
             destination = geocoder.geocode(destination_address)
@@ -100,6 +141,71 @@ def compute():
             destination.latitude, destination.longitude,
         )
 
+    # GTFO IF NO ORIGINS OR DESTINATIONS ######################################
 
+    if 0 == len(origins):
+        response += u"Failed to geolocalize all the origin(s)."
+        _handle_failure(estimation, response)
+        return _respond(response)
+    if 0 == len(destinations):
+        response += u"Failed to geolocalize all the destination(s)."
+        _handle_failure(estimation, response)
+        return _respond(response)
+
+    # RECOVER THE EMISSION MODELS #############################################
+
+    emission_models_confs = content.models
+    emission_models = []
+
+    for model_conf in emission_models_confs:
+        model_file = model_conf.file
+        the_module = importlib.import_module("flaskr.laws.%s" % model_file)
+
+        model = the_module.EmissionModel(model_conf)
+        # model.configure(extra_model_conf)
+
+        emission_models.append(model)
+
+    # print(emission_models)
+
+    # PREPARE RESULT DICTIONARY THAT WILL BE STORED ###########################
+
+    results = {}
+
+    # SCENARIO A : One Origin, At Least One Destination #######################
+    #
+    # In this scenario, we compute the sum of each of the travels' footprint,
+    # for each of the Emission Models, and present a mean of all Models.
+    #
+
+    if 1 == len(origins):
+
+        footprints = {}
+
+        for model in emission_models:
+            cities = {}
+            for destination in destinations:
+                footprint = model.compute_travel_footprint(
+                    origin.latitude, origin.longitude,
+                    destination.latitude, destination.longitude,
+                )
+                cities[destination.address] = footprint
+            footprints[model.config.name] = {
+                'cities': cities,
+            }
+
+        response += repr(footprints)
+
+        results['footprints'] = footprints
+
+    # SCENARIO B : At Least One Origin, One Destination #######################
+    #
+    # Same as A for now.
+    #
+
+    # SCENARIO C : At Least One Origin, At Least One Destination ##############
+    #
+    # Run Scenario A for each Destination, and expose optimum Destination.
+    #
 
     return _respond(response)
