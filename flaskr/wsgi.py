684a0b875436ce8b63b4de80d689d3f573e2a51f
Antoine Goutenoir <antoine.goutenoir@irap.omp.eu>
2019-10-24 18:03:38 +0200

    Configure WSGI.

--- flaskr/wsgi.py
+++ flaskr/wsgi.py
@@ -6,9 +6,10 @@ PROJECT_ROOT_DIR = os.path.abspath(os.path.dirname(os.path.dirname(__file__)))
 sys.path.append(PROJECT_ROOT_DIR)
 
 # Only now can we import locals
+# --> Not really true anymore since this file is in flaskr now.
 from flaskr import create_app
 
 app = create_app('flaskr.settings.ProductionConfig')
 
 if __name__ == "__main__":
-    app.run(host='0.0.0.0')
+    app.run(host='0.0.0.0', port=80)
