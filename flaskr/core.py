5e995dfe90c9ac278ad2dd87526d815530b9326a
Antoine Goutenoir <antoine.goutenoir@irap.omp.eu>
2019-10-30 11:23:04 +0100

    Refactor how we collect the emission models.

--- flaskr/core.py
+++ flaskr/core.py
@@ -1,6 +1,9 @@
 import abc
+import importlib
 from datetime import datetime
 from uuid import uuid4
+
+from .content import content
 
 
 def generate_unique_id():
@@ -10,10 +13,26 @@ def generate_unique_id():
     return datetime.now().strftime('%Y-%m-%d_%H:%M:%S_') + str(uuid4())[0:4]
 
 
-# def estimate_travel_carbon_footprint(from_address, to_address):
-#     return 1
+def get_emission_models():
+    emission_models_confs = content.models
+    emission_models = []
+
+    for model_conf in emission_models_confs:
+        model_file = model_conf.file
+        the_module = importlib.import_module("flaskr.laws.%s" % model_file)
+
+        model = the_module.EmissionModel(model_conf)
+        # model.configure(extra_model_conf)
+
+        emission_models.append(model)
+
+    return emission_models
 
 
+models = get_emission_models()
+
+
+# unused
 class FootprintEstimatorDriver(abc.ABCMeta):
     @abc.abstractmethod
     def get_travel_footprint(self, from_location, to_location):  # TBD
