9b550055ab0164a468c2805a46dda5e119195e19
Antoine Goutenoir <antoine.goutenoir@irap.omp.eu>
2020-02-14 09:13:01 +0100

    Clean up.

--- flaskr/geocoder.py
+++ flaskr/geocoder.py
@@ -10,13 +10,13 @@ class CachedGeocoder:
     def __init__(self, source="Nominatim", geocache="geocache.db"):
         self.geocoder = getattr(geopy.geocoders, source)()
         self.cache = shelve.open(get_path(geocache), writeback=True)
-        self.timestamp = time.time() + 1.5
+        # self.timestamp = time.time() + 1.5
 
     def geocode(self, address):
         if address not in self.cache:
             # time.sleep(max(0, 1 - (time.time() - self.timestamp)))
             time.sleep(1.618)
-            self.timestamp = time.time()
+            # self.timestamp = time.time()
             self.cache[address] = self.geocoder.geocode(
                 query=address,
                 timeout=5,
