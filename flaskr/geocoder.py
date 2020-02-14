e42b2aa7cec3c8a144a2dca8f284e5ad6ac86380
Antoine Goutenoir <antoine.goutenoir@irap.omp.eu>
2020-02-14 09:11:43 +0100

    Use path relativity in the geocoder.
    Don't try to be clever with the interval between OSM queries.

--- flaskr/geocoder.py
+++ flaskr/geocoder.py
@@ -2,17 +2,20 @@ import geopy
 import shelve
 import time
 
+from core import get_path
+
 
 class CachedGeocoder:
 
     def __init__(self, source="Nominatim", geocache="geocache.db"):
         self.geocoder = getattr(geopy.geocoders, source)()
-        self.cache = shelve.open(geocache, writeback=True)
+        self.cache = shelve.open(get_path(geocache), writeback=True)
         self.timestamp = time.time() + 1.5
 
     def geocode(self, address):
         if address not in self.cache:
-            time.sleep(max(0, 1 - (time.time() - self.timestamp)))
+            # time.sleep(max(0, 1 - (time.time() - self.timestamp)))
+            time.sleep(1.618)
             self.timestamp = time.time()
             self.cache[address] = self.geocoder.geocode(
                 query=address,
