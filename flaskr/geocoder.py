bb86c6bf79cf4575f2eb1597b234ca8a52ddad1d
Antoine Goutenoir <antoine.goutenoir@irap.omp.eu>
2024-02-20 02:02:30 +0100

    config: user agent for Nominatim
    /spend 15m

--- flaskr/geocoder.py
+++ flaskr/geocoder.py
@@ -1,9 +1,10 @@
-import certifi
-import geopy
-import geopy.geocoders
 import shelve
 import ssl
 import time
+
+import certifi
+import geopy
+import geopy.geocoders
 
 from flaskr.core import get_path
 
@@ -14,20 +15,20 @@ geopy.geocoders.options.default_ssl_context = ssl_ctx
 class CachedGeocoder:
 
     def __init__(self, source="Nominatim", geocache="geocache.db"):
-        self.geocoder = getattr(geopy.geocoders, source)(scheme='https')
+        self.geocoder = getattr(geopy.geocoders, source)(
+            scheme='https',
+            user_agent='TravelCarbonFootprintCalculator',
+        )
         self.cache = shelve.open(get_path(geocache), writeback=True)
-        # self.timestamp = time.time() + 1.5
 
     def geocode(self, address):
         if address not in self.cache:
-            # time.sleep(max(0, 1 - (time.time() - self.timestamp)))
             time.sleep(1.618)
-            # self.timestamp = time.time()
             self.cache[address] = self.geocoder.geocode(
                 query=address,
                 timeout=5,
-                # language='en_US',
-                language='en',  # urgh
+                # language='en_US',  # buggy since 2024
+                language='en',
                 addressdetails=True,  # only works with Nominatim /!.
             )
         return self.cache[address]
